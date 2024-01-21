84dcd301e272a9d85dce4d321217e01b7ca0495d
Martin Roa Villescas <mroavi@gmail.com>
2024-01-21 19:55:08 +0100

    Update "toggle bar" and "new terminal" key mappings

--- config.def.h
+++ config.def.h
@@ -85,8 +85,8 @@ static const Key keys[] = {
 
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_n,      spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
